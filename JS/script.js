59a1485616673c64ce764827e1f21582f0120dba
Bnjackson <65509144+Bnjackson@users.noreply.github.com>
2020-07-05 16:45:13 +0100

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,12 @@
-alert( "Hello, World!" )
+/*comment across
+multiple line*/
+//comment on one line
+// CTRL / is a hotkey to comment out lines
+"use strict";
+// use strict will enable modern JS features that are off by default
+
+let message;
+message = "Hello, World!";
+
+alert(message);
+//Statements are syntax constructs and commands that perform actions
