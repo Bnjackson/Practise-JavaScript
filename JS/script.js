1a9586f1fc343c01a02967d5120f74f22585d4e8
Bnjackson <65509144+Bnjackson@users.noreply.github.com>
2020-07-05 19:01:59 +0100

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,26 @@
-/*comment across
+/* comment across
 multiple line*/
-//comment on one line
+// comment on one line
 // CTRL / is a hotkey to comment out lines
 "use strict";
 // use strict will enable modern JS features that are off by default
 // Variables are "named storage" for data
-
+/*
 let message; // The let keyword lets us create variables
 message = "Hello, World!"; // stroring data in the variable
+*/
+let messageHello = "Hello, World!",
+age = "24";
+/* we can combine multiple variable declarations and assignments together*/
 
-alert(message); // shows the variable content
-//Statements are syntax constructs and commands that perform actions
+alert(messageHello); // shows the variable content
+alert(age);
+// Statements are syntax constructs and commands that perform actions
+
+// To declare a constant (unchanging) variable we use const instead of let. varibles declared const will never change.
+
+const number = "123";
+
+//a common practise is to use constants as alieses for difficult to remember values that are known beforehand. Such constants are named using capital letters and underscores.
+
+const COLOUR_RED = "#f00";
