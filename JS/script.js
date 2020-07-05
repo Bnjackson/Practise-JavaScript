69f2baae5ad50895afd010d628b2aa6f0639a6eb
Bnjackson <65509144+Bnjackson@users.noreply.github.com>
2020-07-05 16:57:00 +0100

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -4,9 +4,10 @@ multiple line*/
 // CTRL / is a hotkey to comment out lines
 "use strict";
 // use strict will enable modern JS features that are off by default
+// Variables are "named storage" for data
 
-let message;
-message = "Hello, World!";
+let message; // The let keyword lets us create variables
+message = "Hello, World!"; // stroring data in the variable
 
-alert(message);
+alert(message); // shows the variable content
 //Statements are syntax constructs and commands that perform actions
